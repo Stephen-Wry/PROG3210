9116993194876092c912b2d726eb9b6a372fcced
Stephen-Wry <31700703+Stephen-Wry@users.noreply.github.com>
2017-09-15 08:42:46 -0400

    Add files via upload

---
+++ Lecture1/app/src/main/java/ca/on/conestogac/swlecture1/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.on.conestogac.swlecture1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
